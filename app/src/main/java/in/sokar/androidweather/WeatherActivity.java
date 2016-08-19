4d91b326b529f4083d3108c18b4308565b00a8a5
Soumya <SoumyaKanti.Kar@trgc.com>
2016-08-19 15:38:44 -0400

    AndroidWeather
    Weather application created using native development.

---
+++ app/src/main/java/in/sokar/androidweather/WeatherActivity.java
@@ -0,0 +1,12 @@
+package in.sokar.androidweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
